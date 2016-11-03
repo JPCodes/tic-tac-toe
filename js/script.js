de104b18260c818fa15ad1017392054bb9ba6639
Eric Raetz and Jim Padilla <eric@>
2016-11-03 15:44:40 -0700

    add rough winner check

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@ function Player(playerMark) {
 	this.markedSquares = 0;
 }
 
-// function Space(spaceNumber) {
-// 	this.spaceNumber = spaceNumber;
-// }
+
 function Board() {
 	this.spacesCondition = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 	this.boardGrid = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
@@ -40,7 +38,7 @@ Board.prototype.modifySpace = function(markPosition) {
 
 var arrayX = [];
 
-var winnerCheck = function(board) {
+var winnerCheckX = function(board) {
 	debugger;
 
 	for (i = 0; i < newBoard["spacesCondition"].length; i++) {
@@ -119,38 +117,100 @@ for (i = 0; i < win8.length; i++) {
 }
 
 if (somethingVar.indexOf("-") < 0 || somethingVar2.indexOf("-") < 0 || somethingVar3.indexOf("-") < 0 || somethingVar4.indexOf("-") < 0 || somethingVar5.indexOf("-") < 0 || somethingVar6.indexOf("-") < 0 || somethingVar7.indexOf("-") < 0 || somethingVar8.indexOf("-") < 0) {
-  console.log("Win");
+  console.log("Winner X");
 } else {
-  console.log("No win")
+  console.log("Not winner X")
 }
 
 }
 
-// Board.prototype.getX = function() {
-// debugger;
-// var arrayX = [];
-//
-// 	for (i = 0; i < this["spacesCondition"].length; i++) {
-//
-// 		if (this.spacesCondition[i] === "X") {
-// 			arrayX.push(this.spacesCondition.indexOf("X", i));
-// 		}
-// 	}
-//
-// 	var arrayXStr = arrayX.join("");
-//
-// 	for (i = 0; i < arrayXStr.length; i++) {
-// 		console.log(arrayXStr.indexOf(this.winningCombinations[i][i]));
-// 	}
-//
-// 	return arrayX;
-// }
-//
-// Board.prototype.getO = function(spacesCond) {
-// 	var arrayO = [];
-// 	arrayX.push(this.spacesCond === "O");
-// 	return arrayO;
-// }
+var arrayO = [];
+
+var winnerCheckO = function(board) {
+	debugger;
+
+	for (i = 0; i < newBoard["spacesCondition"].length; i++) {
+
+		if (newBoard.spacesCondition[i] === "O") {
+			arrayO.push(newBoard.spacesCondition.indexOf("O", i));
+		}
+	}
+
+	board = arrayO.join("");
+
+var win1 = "048";
+var win2 = "012";
+var win3 = "036";
+var win4 = "147";
+var win5 = "258";
+var win6 = "246";
+var win7 = "345";
+var win8 = "678";
+
+var somethingVar = "";
+var somethingVar2 = "";
+var somethingVar3 = "";
+var somethingVar4 = "";
+var somethingVar5 = "";
+var somethingVar6 = "";
+var somethingVar7 = "";
+var somethingVar8 = "";
+
+for (i = 0; i < win1.length; i++) {
+  if (win1[i] > -1){
+    somethingVar += board.indexOf(win1[i]);
+  }
+}
+
+for (i = 0; i < win2.length; i++) {
+  if (win2[i] > -1){
+    somethingVar2 += board.indexOf(win2[i]);
+  }
+}
+
+for (i = 0; i < win3.length; i++) {
+  if (win3[i] > -1){
+    somethingVar3 += board.indexOf(win3[i]);
+  }
+}
+
+for (i = 0; i < win4.length; i++) {
+  if (win4[i] > -1){
+    somethingVar4 += board.indexOf(win4[i]);
+  }
+}
+
+for (i = 0; i < win5.length; i++) {
+  if (win5[i] > -1){
+    somethingVar5 += board.indexOf(win5[i]);
+  }
+}
+
+for (i = 0; i < win6.length; i++) {
+  if (win6[i] > -1){
+    somethingVar6 += board.indexOf(win6[i]);
+  }
+}
+
+for (i = 0; i < win7.length; i++) {
+  if (win7[i] > -1){
+    somethingVar7 += board.indexOf(win7[i]);
+  }
+}
+
+for (i = 0; i < win8.length; i++) {
+  if (win8[i] > -1){
+    somethingVar8 += board.indexOf(win8[i]);
+  }
+}
+
+if (somethingVar.indexOf("-") < 0 || somethingVar2.indexOf("-") < 0 || somethingVar3.indexOf("-") < 0 || somethingVar4.indexOf("-") < 0 || somethingVar5.indexOf("-") < 0 || somethingVar6.indexOf("-") < 0 || somethingVar7.indexOf("-") < 0 || somethingVar8.indexOf("-") < 0) {
+  console.log("Winner O");
+} else {
+  console.log("Not winner O")
+}
+
+}
 
 function playerTurn() {
 	if (i % 2 === 0) {
@@ -158,6 +218,14 @@ function playerTurn() {
 		return player1.playerMark;
 	} else if (i % 2 === 1) {
 		i++;
+		return player2.playerMark;
+	}
+}
+
+function playerTurn2() {
+	if (i % 2 === 0) {
+		return player1.playerMark;
+	} else if (i % 2 === 1) {
 		return player2.playerMark;
 	}
 }
@@ -195,36 +263,10 @@ var spaceSpot;
 		$("#didIwin").click(function() {
 			debugger;
 			var boardCond = newBoard.spacesCondition;
-			winnerCheck(boardCond);
-			//
-			// newBoard["winningCombinations"].forEach(function(combination){
-			//
-			// 	var currentCombination = combination.join("");
-			// 	var findXs = newBoard.getX().join("");
-			// 		if (currentCombination === findXs) {
-			// 			console.log("Win");
-			// 		}	else if (currentCombination !== newBoard.spacesCondition) {
-			// 			console.log("Nothing");
-			// 		}
-			//
-			// })
+			winnerCheckX(boardCond);
+			winnerCheckO(boardCond);
 
 		})
 
 
 });
-
-// 0, 4, 8 to string, winningCombinations
-// indexOf(0, 4, 8);
-// 0, 3, 4, 8 to string, boards
-// 1, -1, 1, 1
-// remove all -1
-//
-// 048 === 048; yes
-
-// var blah = winnningCombinations.join("");
-// var maxfilter = blah.indexOf();
-// maxfilter.remove(-1);
-// if maxfilter[i] > 2 {
-// 	alert("win win win");
-// }
